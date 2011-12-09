a454d4573d7ceac089484e8deccd7f633a94466d
Chris Proto <cproto@fusionbox.com>
2011-12-09 16:57:25 -0700

    some doc strings for fusionboxApp

--- fusionbox/app.py
+++ fusionbox/app.py
@@ -2,6 +2,10 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 
 class FusionboxApp(object):
+    """
+    Base Class for implementing Fusionbox Apps
+
+    """
     app_name = None
     namespace = None
 
@@ -19,10 +23,18 @@ class FusionboxApp(object):
         raise NotImplementedError()
 
     def redirect(self, to, *args, **kwargs):
+        """
+        Uses namespaced reverse
+        """
         uri = self.reverse(to, args=args, kwargs=kwargs)
         return redirect(uri)
 
     def reverse(self, to, args=None, kwargs=None):
+        """
+        Wrapper for django.core.urlresolvers.reverse
+
+        Calls reverse() based on namespace settings for current app
+        """
         to = '%s:%s' % (self.namespace, to)
         return reverse(to, args=args, kwargs=kwargs, current_app=self.app_name)
 
