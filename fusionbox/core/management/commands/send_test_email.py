cad3d4d57f72223ccf017b9089541658503f59b8
Gavin Wahl <gwahl@fusionbox.com>
2015-02-26 17:00:00 -0700

    remove unused import

--- fusionbox/core/management/commands/send_test_email.py
+++ fusionbox/core/management/commands/send_test_email.py
@@ -2,12 +2,10 @@ import socket, datetime
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import send_mail
-from django.conf import settings
 
 class Command(BaseCommand):
     help = "Sends an email through the framework to an address specified on the command line."
     args = "<email email...>"
-    can_import_settings = True
 
     def handle(self, *args, **kwargs):
         if not args:
