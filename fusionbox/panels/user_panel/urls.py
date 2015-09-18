a60c3f0761dca78b325092988f6072c026452f3e
Ratheesh kumar R <ratheeshspiral09@gmail.com>
2015-09-18 15:22:53 +0530

    Update urls.py

--- fusionbox/panels/user_panel/urls.py
+++ fusionbox/panels/user_panel/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url, include
 
-+_PREFIX = '__debug__'
+_PREFIX = '__debug__'
 
 urlpatterns = patterns('fusionbox.panels.user_panel.views',
     url(r'^%s/users/$' % _PREFIX, 'content',
